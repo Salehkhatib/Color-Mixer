8f2cd7cf11b1ab9e980ff7d7901ab9b9d1b4383d
Salehkhatib <skhatib552@gmail.com>
2022-11-17 14:22:20 +0100

    LocalStorage

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
+const red = document.querySelector("#red");
+const green = document.querySelector("#green");
+const blue = document.querySelector("#blue");
+
 function colors() {
-    const red = document.querySelector("#red").value;
-    const green = document.querySelector("#green").value;
-    const blue = document.querySelector("#blue").value;
-    document.body.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+  document.body.style.backgroundColor =
+    "rgb(" + red.value + "," + green.value + "," + blue.value + ")";
 
-    let hex = "";
+  let hex = "";
 
-    let r = parseInt(red, 10).toString(16);
-    r = r.length == 1 ? "0" + r : r;
+  let r = parseInt(red.value, 10).toString(16);
+  r = r.length == 1 ? "0" + r : r;
 
-    let b = parseInt(green, 10).toString(16);
-    b = b.length == 1 ? "0" + b : b;
+  let b = parseInt(green.value, 10).toString(16);
+  b = b.length == 1 ? "0" + b : b;
 
-    let g = parseInt(blue, 10).toString(16);
-    g = g.length == 1 ? "0" + g : g;
+  let g = parseInt(blue.value, 10).toString(16);
+  g = g.length == 1 ? "0" + g : g;
 
-    hex = "#" + r + b + g;
-    document.querySelector("#hex").innerText = hex;
+  hex = "#" + r + b + g;
+  document.querySelector("#hex").innerText = hex;
 }
-colors()
+colors();
 
+let randomColor = document.querySelector("#random");
+randomColor.addEventListener("click", function () {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => response.json())
+    .then((colors) => {
+      red.value = colors.rgb.r;
+      green.value = colors.rgb.b;
+      blue.value = colors.rgb.g;
+    });
+  colors();
+  localStorage();
+});
 
-
-
-
- let randomColor = document.querySelector("#random")
-     randomColor.addEventListener("click", function () {
-    
-       fetch("https://dummy-apis.netlify.app/api/color")
-      .then((response) => response.json())
-      .then((colors) => {
-        red.value = colors.rgb.r;
-        green.value = colors.rgb.b;
-        blue.value = colors.rgb.g;
-      });
-      colors()
-      localStorage()
-  });
-  
-  function localStorage() {
-    if(localStorage.setItem("colors")) {
-      const qouteElement = localStorage.getItem("colors");
-      todos = JSON.parse(colorsElement);
-    }  
+function localStorage() {
+  if (localStorage.setItem("colors")) {
+    const qouteElement = localStorage.getItem("colors");
+    todos = JSON.parse(colorsElement);
   }
-  localStorage()
-       loadData()
+}
+localStorage();
+loadData();
